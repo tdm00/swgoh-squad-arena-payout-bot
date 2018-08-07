6486f6269885894a99d9cccbf65c5dd0cc55895e
Troy Murray <tm@msu.edu>
2018-08-06 22:13:30 -0400

    Add http response
    Zeit Now seems to have added a restriction where you can no longer keep a hosted application running when it receives no inbound traffic. This adds a http response to any requests (i.e. pingdom)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-require('http').createServer().listen(3000)
-
 import Bot from './Bot'
 import { botToken } from './secret'
 new Bot(botToken)
+
+// require('http').createServer().listen(3000)
+const http = require('http');
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('jedi\n');
+});
+
+server.listen(port)
